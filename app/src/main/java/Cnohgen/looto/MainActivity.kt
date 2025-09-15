c0cbd0407e61d91caa4328042c0bc67c776e07ed
Louis <machongen@gmail.com>
2025-09-15 17:25:35 +0800

    第四次commit (垂直排列)

--- app/src/main/java/Cnohgen/looto/MainActivity.kt
+++ app/src/main/java/Cnohgen/looto/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import Cnohgen.looto.ui.theme.LootoTheme
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 
 class MainActivity : ComponentActivity() {
@@ -34,15 +35,16 @@ class MainActivity : ComponentActivity() {
 fun Play(modifier: Modifier = Modifier) {
     var lucky = (1..100).random()
 
-    Text(
-        text = "樂透數字(1-100)為 $lucky",
-        modifier = modifier
-    )
+    Column(modifier = modifier) { // <-- 將 Text 和 Button 放在 Column 中
+        Text(
+            text = "樂透數字(1-100)為 $lucky"
+        )
 
-    Button(
-        onClick = { lucky = (1..100).random() }
-    ) {
-        Text("重新產生樂透碼")
+        Button(
+            onClick = { lucky = (1..100).random() }
+        ) {
+            Text("重新產生樂透碼")
+        }
     }
 }
 
